0532e0f54058a2e4bef268fe57a74f616ee99443
larisaad <andreea1997_larisa@yahoo.com>
2018-05-24 16:34:04 +0300

    first commit

---
+++ app/src/main/java/com/example/larisa/pmapp/MainActivity.java
@@ -0,0 +1,409 @@
+package com.example.larisa.pmapp;
+
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String DEVICE_ADDRESS = "20:15:03:17:23:32"; //MAC Address of Bluetooth Module
+    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
+    String command; //string variable that will store value to be transmitted to the bluetooth module
+
+    private Button button_up;
+    private Button button_left;
+    private Button button_right;
+    private Button button_down;
+    private Button button_connect;
+    private Button button_stop;
+    private Button button_speedup;
+    private Button button_speeddown;
+    private Button button_pirleft;
+    private Button button_pirright;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("app", "after setting content view");
+
+        button_up = findViewById(R.id.button_up);
+        button_left = findViewById(R.id.button_left);
+        button_right = findViewById(R.id.button_right);
+        button_down = findViewById(R.id.button_down);
+        button_connect = findViewById(R.id.button_connect);
+        button_stop = findViewById(R.id.button_stop);
+        button_speedup = findViewById(R.id.button_speedup);
+        button_speeddown = findViewById(R.id.button_down);
+        button_pirleft = findViewById(R.id.button_pir_left);
+        button_pirright = findViewById(R.id.button_right);
+
+
+        //OnTouchListener code for the up button (button long press)
+        button_up.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN) //MotionEvent.ACTION_DOWN is when you hold a button down
+                {
+                    command = "U";
+
+                    try
+                    {
+                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+                else if(event.getAction() == MotionEvent.ACTION_UP) //MotionEvent.ACTION_UP is when you release a button
+                {
+                    command = "C";
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch(IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                return false;
+            }
+
+        });
+
+
+        //OnTouchListener code for the down button (button long press)
+        button_down.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    command = "D";
+
+
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+                else if(event.getAction() == MotionEvent.ACTION_UP)
+                {
+                    command = "C";
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch(IOException e)
+                    {
+
+                    }
+
+                }
+                return false;
+            }
+        });
+
+        //OnTouchListener code for the forward left button (button long press)
+        button_left.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    command = "L";
+
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+                else if(event.getAction() == MotionEvent.ACTION_UP)
+                {
+                    command = "C";
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch(IOException e)
+                    {
+
+                    }
+
+                }
+                return false;
+            }
+        });
+
+        //OnTouchListener code for the forward right button (button long press)
+        button_right.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    command = "R";
+
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+                else if(event.getAction() == MotionEvent.ACTION_UP)
+                {
+                    command = "C";
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch(IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                }
+                return false;
+            }
+        });
+
+        //OnTouchListener code for the speed up button (button long press)
+        button_speedup.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    command = "f";
+
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+            return false;
+            }
+        });
+
+        button_speeddown.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    command = "s";
+
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                return false;
+            }
+        });
+
+        button_pirleft.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    command = "a";
+
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                return false;
+            }
+        });
+        button_pirright.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    command = "e";
+
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                return false;
+            }
+        });
+
+        button_stop.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    command = "C";
+
+                    try
+                    {
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                return false;
+            }
+        });
+
+        //Button that connects the device to the bluetooth module when pressed
+        button_connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(BTinit())
+                {
+                    BTconnect();
+                }
+
+            }
+        });
+    }
+
+    //Initializes bluetooth module
+    public boolean BTinit()
+    {
+        boolean found = false;
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(bluetoothAdapter == null) //Checks if the device supports bluetooth
+        {
+            Toast.makeText(getApplicationContext(), "Device doesn't support bluetooth", Toast.LENGTH_SHORT).show();
+        }
+
+        if(!bluetoothAdapter.isEnabled()) //Checks if bluetooth is enabled. If not, the program will ask permission from the user to enable it
+        {
+            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableAdapter,0);
+
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+
+        if(bondedDevices.isEmpty()) //Checks for paired bluetooth devices
+        {
+            Toast.makeText(getApplicationContext(), "Please pair the device first", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            for(BluetoothDevice iterator : bondedDevices)
+            {
+                if(iterator.getAddress().equals(DEVICE_ADDRESS))
+                {
+                    device = iterator;
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        return found;
+    }
+
+    public boolean BTconnect()
+    {
+        boolean connected = true;
+
+        try
+        {
+            socket = device.createRfcommSocketToServiceRecord(PORT_UUID); //Creates a socket to handle the outgoing connection
+            socket.connect();
+
+            Toast.makeText(getApplicationContext(),
+                    "Connection to bluetooth device successful", Toast.LENGTH_LONG).show();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            connected = false;
+        }
+
+        if(connected)
+        {
+            try
+            {
+                outputStream = socket.getOutputStream(); //gets the output stream of the socket
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return connected;
+    }
+}
